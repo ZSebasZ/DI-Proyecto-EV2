a2bfaf1298aedfea6c9510a58f196d7e34d2f2ac
Sebastian Jimenez <sarboledacarballeira@gmail.com>
2025-03-05 12:12:22 +0100

    Commit 7 - Pequeñas correcciones

--- src/componentes/ListaTareas.js
+++ src/componentes/ListaTareas.js
@@ -9,15 +9,41 @@ const ListaTareas = ({ tareas, filtro, dispatch }) => {
 
     return (
         <div id='contenedorTareas'>
-            {/*Recorremos la constante con las tareas filtradas o no y retornamos un componente Tarea,
-            al cual le asignamos su 'key', le pasamos la tarea y el dispatch*/}
-            {tareasFiltradas.map((tarea) => (
-                <Tarea 
-                    key={tarea.id} 
-                    tarea={tarea} 
-                    dispatch={dispatch}
-                />
-            ))}
+            {/*En caso de que 'tareas' tenga tareas entramos por este bloque*/}
+            {tareas.length > 0 ? (
+                <>
+                    {/*En caso de que 'tareasFiltradas' tenga tareas entramos por este bloque*/}
+                    {tareasFiltradas.length > 0 ? (
+                        <>
+                            {/*Recorremos la constante con las tareas filtradas y retornamos un componente Tarea,
+                            al cual le asignamos su 'key', le pasamos la tarea y el dispatch*/}
+                            {tareasFiltradas.map((tarea) => (
+                                <Tarea
+                                    key={tarea.id}
+                                    tarea={tarea}
+                                    dispatch={dispatch}
+                                />
+                            ))}
+                        </>
+                    ) : (
+                        //De lo contrario, mostramos este mensaje
+                        <>
+                            <div className='containerNoTareas filtradas'>
+                                <p>Si no ves tus tareas es porque todas estan completadas y el filtro solo esta mostrando las no completadas</p>
+                            </div>
+                        </>
+                    )}
+                </>
+            ) : (
+                //De lo contrario, mostramos este mensaje
+                <>
+                <div className='containerNoTareas'>
+                    <p>Aun no tienes tareas</p>
+                </div>
+                </>
+            )}
+
+
         </div>
     );
 }
